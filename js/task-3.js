a2f3dbef001acc73ffb3024dbc4501f79cfc150f
Roman_Firtsovych <r.firtsovych@gmail.com>
2024-08-18 11:04:54 +0300

    5

--- js/task-3.js
+++ js/task-3.js
@@ -5,3 +5,6 @@ const contentWidth = parseFloat(content);
 const totalWidth = contentWidth + paddingWidth + borderWidth;
 return totalWidth;
 }
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
